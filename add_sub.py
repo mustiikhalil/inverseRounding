9c2d33860c3920bb3ef28324e24641fc5573f209
Mustafa Khalil <mustii@Mustafas-MacBook-Pro.local>
2017-12-10 22:10:04 +0300

    add and sub

--- add_sub.py
+++ add_sub.py
@@ -1,44 +1,21 @@
 
 
 def add(number1,number2):
-    x = str(number1)
-    y = str(number2)
-    num1 = []
-    for i in x:
-        num1.append(i)
-
+    num1 = str(number1)
+    num2 = str(number2)
     num1 = num1[len(num1)//2:]
+    num1 += num2
+    return int(num1)
 
-    for i in y:
-        num1.append(i)
-    stringNum = ''
-    for i in num1:
-        stringNum += i
-
-    return int(stringNum)
-
-
-print(add(214,6))
+print(add(212334,623))
 
 
 def sub(number1,number2):
-    x = str(number1)
-    y = str(number2)
-    num1 = []
-    num2 = []
-    for i in x:
-        num1.append(i)
-
-    num1 = num1[len(num1)//2:]
-
-    for i in y:
-        num2.append(i)
+    num1 = str(number1)
+    num2 = str(number2)
+    num1 = num1[len(num1) // 2:]
     num1 = num2 + num1
-
-    stringNum = ''
-    for i in num1:
-        stringNum += i
-
-    return int(stringNum)
+    return int(num1)
 
 print(sub(214,6))
+
